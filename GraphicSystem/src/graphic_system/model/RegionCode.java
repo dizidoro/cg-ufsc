d78bbfdc322ff2d35d292771efae52a11ce28293
= <diegolweiss@gmail.com>
2014-04-24 09:08:56 -0300

    Arquivo RegionCode

---
+++ GraphicSystem/src/graphic_system/model/RegionCode.java
@@ -0,0 +1,15 @@
+package graphic_system.model;
+
+public class RegionCode {
+	
+	int top, bottom, right, left, all;
+	
+	RegionCode() {
+		top = 0;
+		bottom = 0;
+		right = 0;
+		left = 0;
+		all = 0;
+	}
+	
+}
